9aa954cef7ee9a4b853a251fc796e05c47a42124
nandasino <fernandamsino@gmail.com>
2022-09-23 20:24:03 -0300

    Aparece a palavra a ser adivinhada na tela, com um underline ( _ ) e palavra sendo sorteada

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { useState } from "react"
 import palavras from './palavras';
 
 
 export default function App() {
+    function comparador() { 
+        return Math.random() - 0.5; 
+    }
+    const [palavra, setPalavra] = useState([])
+    function escolherPalavra(){
+        const embalharado = palavras.sort(comparador);
+        const novaPalavra= embalharado[0];
+        const palavraEmArray= novaPalavra.split("");
+        console.log(palavraEmArray);
+        setPalavra(palavraEmArray);
+    }
+
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     return (
         <div className='conteudo'>
             <div className='forca'>
                 <img src='/assets/forca0.png'></img>
-                <button>Escolher a palavra</button>
+                <button onClick={escolherPalavra}>Escolher a palavra</button>
+                {palavra.map((p)=> "_ ")}
             </div>
-            <div className='teclado'>{alfabeto.map((l)=><div className='botao'>{l}</div>)}</div>
+            <div className='teclado'>{alfabeto.map((l,index)=><div className='botao'>{l}</div>)}</div>
             <div className='chute'><p>Já sei a palavra</p><input></input><button>Chutar</button></div>
         </div>
     );
