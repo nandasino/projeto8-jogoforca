9a89ed2393a100d103776ba81e50218f3abb0d8c
nandasino <fernandamsino@gmail.com>
2022-09-24 18:18:26 -0300

    quantidades de vidas altera conforme eu clico na letra

--- src/App.js
+++ src/App.js
@@ -5,25 +5,35 @@ import palavras from './palavras';
 export default function App() {
     function comparador() { 
         return Math.random() - 0.5; 
-    }
-    const [palavra, setPalavra] = useState([])
+    } 
+    const [vida,setVida] = useState(6);
+    const [palavra, setPalavra] = useState([]);
     function escolherPalavra(){
         const embalharado = palavras.sort(comparador);
         const novaPalavra= embalharado[0];
         const palavraEmArray= novaPalavra.split("");
-        console.log(palavraEmArray);
         setPalavra(palavraEmArray);
     }
-
+    function tentarLetra(letra){
+        console.log(palavra);
+        if(palavra.includes(letra)){
+            console.log("tem essa letra");
+        }
+        else{
+            console.log("não tem essa letra");
+            setVida(vida-1);
+        }
+    }
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     return (
         <div className='conteudo'>
             <div className='forca'>
                 <img src='/assets/forca0.png'></img>
+                <p>Vidas: {vida}</p>
                 <button onClick={escolherPalavra}>Escolher a palavra</button>
                 {palavra.map((p)=> "_ ")}
             </div>
-            <div className='teclado'>{alfabeto.map((l,index)=><div className='botao'>{l}</div>)}</div>
+            <div className='teclado'>{alfabeto.map((l,index)=><div onClick={()=>tentarLetra(l)} className='botao'>{l}</div>)}</div>
             <div className='chute'><p>Já sei a palavra</p><input></input><button>Chutar</button></div>
         </div>
     );
